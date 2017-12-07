8df4d7a2b510c3ff446c47ebdf6671d6392a32ff
kenFlowers <kenneth_flow@Hotmail.com>
2017-12-07 09:15:30 -0600

    final

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'IgXoyL4iTNBCuES3zPchYvSis',
+  consumer_secret: 'I1vHXZfBlrJJl5UosIL0a72tGq1XZ5fFXMnvgdpENZfXUZo63q',
+  access_token_key: '935893903420674048-BJR3T2SIcTTv07TXGzC666y7N7ZO1Q2',
+  access_token_secret: '5tTGGrqe6dtwNK5ku8R2sER0A4Xhorp4ghJxGJD8v64hn',
+}
+
+module.exports = twitterKeys;
